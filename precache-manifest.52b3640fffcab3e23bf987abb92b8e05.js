c51be017ffb56a1cac74285feb22f10d388f7765
AmplifiedHuman <amplifiedhuman@gmail.com>
2020-09-01 16:34:29 +0800

    Updates

---
+++ precache-manifest.52b3640fffcab3e23bf987abb92b8e05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63e9d827b0e783a6b5afa43e5fefcc2",
+    "url": "/face-detect/index.html"
+  },
+  {
+    "revision": "76a2621a8165eb23e57c",
+    "url": "/face-detect/static/css/main.d29312d9.chunk.css"
+  },
+  {
+    "revision": "3d0591ac290c0589c7fa",
+    "url": "/face-detect/static/js/2.4ec08843.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-detect/static/js/2.4ec08843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a2621a8165eb23e57c",
+    "url": "/face-detect/static/js/main.41a078d0.chunk.js"
+  },
+  {
+    "revision": "222e7e7060c17ee90085",
+    "url": "/face-detect/static/js/runtime-main.e8a0c4bc.js"
+  }
+]);
